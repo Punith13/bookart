8046f26c1125d558d32f807e89dbc18c78fe066c
Punith Ujwal Nayak <punith.ujwal.n.nayak@ericsson.com>
2017-03-22 16:23:29 +0530

    Adding middleware to remove subdocuments

--- models/user.js
+++ models/user.js
@@ -17,6 +17,21 @@ orders : [{
     
 });
 
+
+// middleware : functions that are run pre and post of events - init , validate , save , remove 
+UserSchema.pre('remove' , function(next){
+    const Address = mongoose.model('address');  
+    
+    Address.remove({ _id : { $in : this.addresses }})  
+    .then( () => next());
+    
+    const Order = mongoose.model('order'); 
+    
+    Order.remove({ _id : { $in : this.orders }})
+      .then ( () => next());
+    
+});
+
 const User = mongoose.model('user' , UserSchema); 
 
 module.exports = User; 
